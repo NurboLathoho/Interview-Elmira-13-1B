e5b7ce929dd873d44f3c389c245689721d15962a
Elmira2007 <moldosakovaelmira@gmail.com>
2024-03-07 18:31:56 +0600

    up

---
+++ test.py
@@ -0,0 +1,24 @@
+# Написать функцию, которая выводит на экран циклом пять строк из нулей, причем каждая строка должна быть пронумерована. 1
+# Дается 2 строки "Aidana" и "Adilet" . Вам нужно в результате получить смешанную строку из двух имен, буква за буквой. Результат: "AAiddialneat” 1
+# Напишите функцию, которая проверяет, сколько раз каждое слово в переданной фразе было использовано. Например: “Money, money, money, it’s always sunny, in 
+# the richmen’s world”. money: 3, it’s: 1, always: 1, sunny: 1, in: 1, the: 1, richmen’s: 1, world: 1.
+
+# 1
+# for i in range(5):
+#     print(f"{i}:: 0")
+
+# 2
+# str1 = "Aidana"
+# str2 = "Adilet"
+# result = ''
+
+# for i in range(max(len(str1), len(str2))):
+#     if i < len(str1):
+#         result += str1[i]
+#     if i < len(str2):
+#         result += str2[i]
+
+# print(result)
+
+
+# 3
